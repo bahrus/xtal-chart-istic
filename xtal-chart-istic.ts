feeb82ec5d43762e1c57c8f56384a099779bd595
bahrus <anderson.bruce.b@gmail.com>
2017-10-29 12:03:16 -0400

    simplify

--- xtal-chart-istic.ts
+++ xtal-chart-istic.ts
@@ -71,6 +71,20 @@ export interface IXtalChartisticProperties{
                 };
 
             }
+
+            static get template(){
+                return `
+    <link rel="stylesheet" type="text/css" href="[[cssPath]]">
+    <style include="xtal-chart-istic-theme"></style>
+    <style>
+        :host {
+        display: block;
+        @apply --my-custom-chart-istic-mixin;
+        }
+    </style>
+    <div id="chartTarget"></div>
+                `;
+            }
             // _stampTemplate(template){
             //     console.log('before stamp template')
             //     super._stampTemplate(template);
